4216c0ed3c02cc9a23d622f5ae70e1b1ced0acf2
Jarno Luukkonen <luukkonen.jarno@gmail.com>
2017-03-23 17:37:56 +0200

    fix

--- deploy.sh
+++ deploy.sh
@@ -8,14 +8,14 @@ source $DIR/config/environment.sh
 echo "Siirretään tiedostot users-palvelimelle..."
 
 # Tämä komento siirtää tiedostot palvelimelta
-rsync -z -r $DIR/app $DIR/assets $DIR/config $DIR/lib $DIR/sql $DIR/vendor $DIR/index.php $DIR/composer.json $USERNAME@$SERVER:htdocs/$PROJECT_FOLDER
+rsync -z -r -e 'ssh -p $PORT' $DIR/app $DIR/assets $DIR/config $DIR/lib $DIR/sql $DIR/vendor $DIR/index.php $DIR/composer.json $USERNAME@$SERVER:htdocs/$PROJECT_FOLDER
 
 echo "Valmis!"
 
 echo "Suoritetaan komento php composer.phar dump-autoload..."
 
 # Suoritetaan php composer.phar dump-autoload
-ssh $USERNAME@$SERVER "
+ssh $USERNAME@$SERVER -p $PORT "
 cd htdocs/$PROJECT_FOLDER
 php composer.phar dump-autoload
 exit"
