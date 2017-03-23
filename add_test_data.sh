6f430c96939fd333aaaab38a62759efa344ac2c8
Jarno Luukkonen <luukkonen.jarno@gmail.com>
2017-03-23 17:45:36 +0200

    fix

--- add_test_data.sh
+++ add_test_data.sh
@@ -4,7 +4,7 @@ source config/environment.sh
 
 echo "Lisätään testidata..."
 
-ssh $USERNAME@$SERVER -p $PORT"
+ssh $USERNAME@$SERVER -p $PORT "
 cd htdocs/$PROJECT_FOLDER/sql
 psql < add_test_data.sql
 exit"
